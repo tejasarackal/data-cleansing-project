39a89c7c8c4500a12e50a451369db7235a0e6745
tarackal <tejas.arackal@segmint.com>
2017-09-02 14:11:28 -0400

    Created the run analysis script for cleaning and merging the required data set

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+# Import Libraries required for the analysis
+# Download the data file and unzip into the data directory
+# Read activity label and feature list
+# Extract data on mean and std deviation features 
+# Cleanse the feature list
+
+# Load the training data sets from sub folders and bind them
+# Load the test data sets from sub folders and bind them
+
+# Merge the training and the test datasets 
+# Add the labels to the merged data with discriptive variable names
+
+# Create second tidy data set grouping activity and subject
+# Find the average for each group and bind it to tidy dataset
