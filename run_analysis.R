ca0f9de109e94b709ffbb318317941b6703dcd24
tarackal <tejas.arackal@segmint.com>
2017-09-02 15:11:39 -0400

    Added code to load training anfninning and test dataset, merge them and reduce labels for required features only

--- run_analysis.R
+++ run_analysis.R
@@ -13,19 +13,33 @@ featureList <- read.table('./UCI HAR Dataset/features.txt')
 
 # Extract data on mean and std deviation features 
 requiredFeatures <- grep("*mean*|*std*", featureList[,2])
-requiredFeaturesList <- featureList[requiredFeatures,2]
+requiredFeaturesNameList <- featureList[requiredFeatures,2]
 
-# Cleanse the feature list
-requiredFeaturesList <- gsub('-mean', ' Mean', requiredFeaturesList)
-requiredFeaturesList <- gsub('-std', ' Std', requiredFeaturesList)
-requiredFeaturesList <- gsub('[()]', '', requiredFeaturesList)
-requiredFeaturesList <- gsub('[-]', ' ', requiredFeaturesList)
+# Cleanse the required feature list
+requiredFeaturesNameList <- gsub('-mean', ' Mean', requiredFeaturesNameList)
+requiredFeaturesNameList <- gsub('-std', ' Std', requiredFeaturesNameList)
+requiredFeaturesNameList <- gsub('[()]', '', requiredFeaturesNameList)
+requiredFeaturesNameList <- gsub('[-]', ' ', requiredFeaturesNameList)
 
 # Load the training data sets from sub folders and bind them
+trainSubject <- read.table('./UCI HAR Dataset/train/subject_train.txt')
+trainActivities <- read.table('./UCI HAR Dataset/train/y_train.txt')
+trainX <- read.table('./UCI HAR Dataset/train/X_train.txt')
+
+train <- cbind(trainSubject, trainActivities, trainX[requiredFeatures])
+
 # Load the test data sets from sub folders and bind them
+testSubject <- read.table('./UCI HAR Dataset/test/subject_test.txt')
+testActivities <- read.table('./UCI HAR Dataset/test/y_test.txt')
+testX <- read.table('./UCI HAR Dataset/test/X_test.txt')
+
+test <- cbind(testSubject, testActivities, testX[requiredFeatures])
 
 # Merge the training and the test datasets 
+mergedSet <- rbind(train, test)
+
 # Add the labels to the merged data with discriptive variable names
+colnames(mergedSet) <- c("subject", "activity", requiredFeaturesNameList)
 
 # Create second tidy data set grouping activity and subject
 # Find the average for each group and bind it to tidy dataset
